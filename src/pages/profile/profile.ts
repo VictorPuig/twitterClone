844d2a1b4a9ea179299f6011dda43a8d16c4f21a
VictorPuig <vicpuigcer@gmail.com>
2017-06-11 23:00:30 +0200

    ProfilePage done

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { User } from '../../clases/user.class';
 import { DataService } from '../../providers/data/data.service';
@@ -8,7 +8,28 @@ import { DataService } from '../../providers/data/data.service';
 })
 export class ProfilePage {
   user: User;
+  @Input() leftPosition: boolean;
+
+  styleClasses: any;
+  title: string;
+
   constructor(public navCtrl: NavController, public dataService: DataService) {
+    this.title = "Profile";
+    this.leftPosition = true;
     this.user = this.dataService.getUser();
+  }
+
+  getStyleClasses() {
+    this.styleClasses = {
+      'Spanish': this.leftPosition,
+      'Arabian': !this.leftPosition
+    }
+    return this.styleClasses;
+  }
+
+  togglePosition (event) {
+    console.log("ADWAWD")
+
+    this.leftPosition = event;
   }
 }
